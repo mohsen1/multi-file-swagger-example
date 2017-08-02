0b2429f957a437444592ea4dccf72da2bf7c83d2
Thomas Nelson <tanelso2@gmail.com>
2017-08-01 17:19:39 -0700

    Added option to output as yaml file
    Also switched from using 'yaml-js' to using 'js-yaml' because the original one
    does not dump nested objects correctly
    
    Tested locally

--- README.md
+++ README.md
@@ -8,17 +8,23 @@ Example of how to split up a large Swagger spec into smaller files.
 ### Usage
 Install the node tool:
 
-```javascript
+```bash
   npm install -g multi-file-swagger
 ```
 
 Run the command like so:
 
-```javascript
+```bash
   multi-file-swagger index.yaml > index.json
 ```
 
 This will resolve every json pointer ($ref) externally or internally and then save it in a json file. Which can then be used for code generation and so on.
+
+
+You can also output as yaml by using the `--output-format` option
+```bash
+  multi-file-swagger -o yaml index.yaml > compiled.yaml
+```
 
 #### License
 MIT
--- resolve.js
+++ resolve.js
@@ -3,15 +3,23 @@
 'use strict';
 
 var resolve = require('json-refs').resolveRefs;
-var YAML = require('yaml-js');
+var YAML = require('js-yaml');
 var fs = require('fs');
 
 var program = require('commander');
 
 program
   .version('2.0.0')
+  .option('-o --output-format [output]',
+          'output format. Choices are "json" and "yaml" (Default is json)',
+          'json')
   .usage('[options] <yaml file ...>')
   .parse(process.argv);
+
+if (program.outputFormat !== 'json' && program.outputFormat !== 'yaml') {
+  console.error(program.help());
+  process.exit(1);
+}
 
 var file = program.args[0];
 
@@ -20,15 +28,19 @@ if (!fs.existsSync(file)) {
   process.exit(1);
 }
 
-var root = YAML.load(fs.readFileSync(file).toString());
+var root = YAML.safeLoad(fs.readFileSync(file).toString());
 var options = {
   filter        : ['relative', 'remote'],
   loaderOptions : {
     processContent : function (res, callback) {
-      callback(null, YAML.load(res.text));
+      callback(null, YAML.safeLoad(res.text));
     }
   }
 };
 resolve(root, options).then(function (results) {
-  console.log(JSON.stringify(results.resolved, null, 2));
+  if (program.outputFormat === 'yaml') {
+    console.log(YAML.safeDump(results.resolved));
+  } else if (program.outputFormat === 'json') {
+    console.log(JSON.stringify(results.resolved, null, 2));
+  }
 });
